67b96331ca8e12de0b3c6723e23d4604ff4b934b
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2015-03-12 21:33:37 +0100

    Fixed a bug which stopped the parsing process of an unmodified file which was read for the second time.
    Prepared the primitive dialog.

---
+++ src/org/nschmidt/ldparteditor/composites/CompositePrimitive.java
@@ -0,0 +1,29 @@
+/* MIT - License
+
+Copyright (c) 2012 - this year, Nils Schmidt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+package org.nschmidt.ldparteditor.composites;
+
+import org.eclipse.swt.widgets.Composite;
+import org.nschmidt.ldparteditor.i18n.I18n;
+
+public class CompositePrimitive extends Composite {
+
+    public CompositePrimitive(Composite parent) {
+        super(parent, I18n.I18N_NON_BIDIRECT());
+        // TODO Auto-generated constructor stub
+    }
+    // FIXME Needs implementation!
+
+}
