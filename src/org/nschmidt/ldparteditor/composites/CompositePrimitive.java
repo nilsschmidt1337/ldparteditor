d279df735fecc13490678c930bb2fe346c9b21da
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2015-03-13 19:46:59 +0100

    Prepared the primitive dialog (3)

--- src/org/nschmidt/ldparteditor/composites/CompositePrimitive.java
+++ src/org/nschmidt/ldparteditor/composites/CompositePrimitive.java
@@ -21,22 +21,56 @@ import org.eclipse.swt.dnd.DragSource;
 import org.eclipse.swt.dnd.DragSourceEvent;
 import org.eclipse.swt.dnd.DragSourceListener;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.opengl.GLCanvas;
+import org.eclipse.swt.opengl.GLData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.GLContext;
 import org.nschmidt.ldparteditor.dnd.MyDummyTransfer2;
 import org.nschmidt.ldparteditor.dnd.MyDummyType2;
 import org.nschmidt.ldparteditor.i18n.I18n;
+import org.nschmidt.ldparteditor.opengl.OpenGLRenderer;
+import org.nschmidt.ldparteditor.opengl.OpenGLRendererPrimitives;
 
 public class CompositePrimitive extends Composite {
+
+    /** The {@linkplain OpenGLRenderer} instance */
+    private final OpenGLRendererPrimitives openGL = new OpenGLRendererPrimitives(this);
+
+    /** the {@linkplain GLCanvas} */
+    final GLCanvas canvas;
 
     public CompositePrimitive(Composite parent) {
         super(parent, I18n.I18N_NON_BIDIRECT() | SWT.BORDER);
         // TODO Auto-generated constructor stub
 
+        this.setLayout(new FillLayout());
+        GLData data = new GLData();
+        data.doubleBuffer = true;
+        data.depthSize = 24;
+        data.alphaSize = 8;
+        data.blueSize = 8;
+        data.redSize = 8;
+        data.greenSize = 8;
+        data.stencilSize = 8;
+        canvas = new GLCanvas(this, I18n.I18N_NON_BIDIRECT(), data);
+        canvas.setCurrent();
+        canvas.setCursor(new Cursor(Display.getCurrent(), SWT.CURSOR_HAND));
+        canvas.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+
+        this.setBackgroundMode(SWT.INHERIT_FORCE);
+
 
         Transfer[] types = new Transfer[] { MyDummyTransfer2.getInstance() };
         int operations = DND.DROP_MOVE | DND.DROP_COPY | DND.DROP_LINK;
 
-        final DragSource source = new DragSource(this, operations);
+        final DragSource source = new DragSource(canvas, operations);
         source.setTransfer(types);
         source.addDragListener(new DragSourceListener() {
             @Override
@@ -52,6 +86,24 @@ public class CompositePrimitive extends Composite {
             @Override
             public void dragFinished(DragSourceEvent event) {
 
+            }
+        });
+
+        try {
+            GLContext.useContext(canvas);
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+        // MARK Resize
+        canvas.addListener(SWT.Resize, new Listener() {
+            @Override
+            public void handleEvent(Event event) {
+                canvas.setCurrent();
+                try {
+                    GLContext.useContext(canvas);
+                } catch (LWJGLException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
