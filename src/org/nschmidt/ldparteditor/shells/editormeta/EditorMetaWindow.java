fd585e35bd9a02dd6fcf46ba2eb44d4f67a5024b
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2015-08-23 14:25:14 +0200

    Updated version number to 0.8.2d

--- src/org/nschmidt/ldparteditor/shells/editormeta/EditorMetaWindow.java
+++ src/org/nschmidt/ldparteditor/shells/editormeta/EditorMetaWindow.java
@@ -739,6 +739,20 @@ public class EditorMetaWindow extends EditorMetaDesign {
             }
         });
 
+        ev_csgEpsilonT_txt[0].addFocusListener(new org.eclipse.swt.events.FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                updateCSGepsilonT();
+            }
+        });
+
+        ev_csgEpsilonT_txt[0].addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent e) {
+                updateCSGepsilonT();
+            }
+        });
+
         ev_csgCompile_txt[0].addFocusListener(new org.eclipse.swt.events.FocusAdapter() {
             @Override
             public void focusGained(FocusEvent e) {
@@ -885,6 +899,11 @@ public class EditorMetaWindow extends EditorMetaDesign {
 
     private void updateCSGepsilon() {
         lbl_lineToInsert[0].setText("0 !LPE CSG_EPSILON " + ev_csgEpsilon_txt[0].getText().trim()); //$NON-NLS-1$
+        lbl_lineToInsert[0].getParent().layout();
+    }
+
+    private void updateCSGepsilonT() {
+        lbl_lineToInsert[0].setText("0 !LPE CSG_EPSILON_T_JUNCTION " + ev_csgEpsilonT_txt[0].getText().trim()); //$NON-NLS-1$
         lbl_lineToInsert[0].getParent().layout();
     }
 
