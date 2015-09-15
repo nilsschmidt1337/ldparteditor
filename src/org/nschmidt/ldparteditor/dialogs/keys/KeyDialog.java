0f525f4ed2c0f30a6a1456964b8252d9c7a215a8
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2015-09-16 00:25:43 +0200

    Prepared fix for issue #67

--- src/org/nschmidt/ldparteditor/dialogs/keys/KeyDialog.java
+++ src/org/nschmidt/ldparteditor/dialogs/keys/KeyDialog.java
@@ -15,7 +15,13 @@ FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TOR
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 package org.nschmidt.ldparteditor.dialogs.keys;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
+import org.nschmidt.ldparteditor.helpers.KeyBoardHelper;
+import org.nschmidt.ldparteditor.state.KeyStateManager;
 
 public class KeyDialog extends KeyDesign {
 
@@ -27,6 +33,30 @@ public class KeyDialog extends KeyDesign {
     public int open() {
         super.create();
         // TODO Add listeners here
+        this.dialogArea.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                final int stateMask = e.stateMask;
+                final int keyCode = e.keyCode;
+                final boolean ctrlPressed = (stateMask  & SWT.CTRL) != 0;
+                final boolean altPressed = (stateMask & SWT.ALT) != 0;
+                final boolean shiftPressed = (stateMask & SWT.SHIFT) != 0;
+                final Event event = new Event();
+                event.keyCode = keyCode;
+                if (ctrlPressed) event.stateMask = event.stateMask | SWT.CTRL;
+                if (altPressed) event.stateMask = event.stateMask | SWT.ALT;
+                if (shiftPressed) event.stateMask = event.stateMask | SWT.SHIFT;
+                lbl_PressKey[0].setText(KeyBoardHelper.getKeyString(event));
+                lbl_PressKey[0].update();
+                KeyStateManager.tmp_keyCode = keyCode;
+                KeyStateManager.tmp_stateMask = stateMask;
+            }
+        });
+
         return super.open();
     }
 }
