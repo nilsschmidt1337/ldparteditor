a1b1fb75274224b73ee21aaa468e0f630c3c848a
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2015-09-15 21:55:10 +0200

    Prepared fix for issue #67

--- src/org/nschmidt/ldparteditor/dialogs/keys/KeyDesign.java
+++ src/org/nschmidt/ldparteditor/dialogs/keys/KeyDesign.java
@@ -15,6 +15,59 @@ FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TOR
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 package org.nschmidt.ldparteditor.dialogs.keys;
 
-class KeyDesign {
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.nschmidt.ldparteditor.i18n.I18n;
 
+class KeyDesign extends Dialog {
+
+
+    final Label[] lbl_PressKey = new Label[1];
+
+    protected KeyDesign(Shell parentShell) {
+        super(parentShell);
+    }
+
+    /**
+     * Create contents of the dialog.
+     *
+     * @param parent
+     */
+    @Override
+    protected Control createDialogArea(final Composite parent) {
+        this.getShell().setText(I18n.KEYBOARD_CustomiseShortkeys);
+        Composite cmp_container = parent;
+        cmp_container.setLayout(new GridLayout());
+
+        Label lbl_PressKey = new Label(cmp_container, I18n.I18N_RTL());
+        this.lbl_PressKey[0] = lbl_PressKey;
+        lbl_PressKey.setText(I18n.KEYBOARD_PressAKey);
+
+        return cmp_container;
+    }
+
+    /**
+     * Create contents of the button bar.
+     *
+     * @param parent
+     */
+    @Override
+    protected void createButtonsForButtonBar(Composite parent) {
+        createButton(parent, IDialogConstants.OK_ID, I18n.DIALOG_OK, true);
+        createButton(parent, IDialogConstants.CANCEL_ID, I18n.DIALOG_Cancel, false);
+    }
+
+    /**
+     * Return the initial size of the dialog.
+     */
+    @Override
+    protected Point getInitialSize() {
+        return super.getInitialSize();
+    }
 }
