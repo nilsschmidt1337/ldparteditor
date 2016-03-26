d4d4700bd40f017a8562c9eac1b87be54a19bbf9
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2016-03-26 22:11:59 +0100

    Prepared fix for issue #200.

--- src/org/nschmidt/ldparteditor/text/DatParser.java
+++ src/org/nschmidt/ldparteditor/text/DatParser.java
@@ -1082,7 +1082,7 @@ public enum DatParser {
                         Matrix4f.mul(productMatrix, tMatrix, destMatrix);
 
                         result.add(new ParsingResult(new GData1(colour.getColourNumber(), colour.getR(), colour.getG(), colour.getB(), colour.getA(), tMatrix, TMatrix, lines, absoluteFilename, sb
-                                .toString(), depth, det < 0, destMatrix, DESTMatrix, datFile, parent.firstRef, readOnly, errorCheckOnly, alreadyParsed, parent, lineNumber)));
+                                .toString(), depth, det < 0, destMatrix, DESTMatrix, datFile, parent.firstRef, readOnly, errorCheckOnly, alreadyParsed, parent)));
                         GDataCSG.forceRecompile();
                     }
 
@@ -1141,7 +1141,7 @@ public enum DatParser {
                         Matrix4f.mul(productMatrix, tMatrix, destMatrix);
 
                         result.add(new ParsingResult(new GData1(colour.getColourNumber(), colour.getR(), colour.getG(), colour.getB(), colour.getA(), tMatrix, TMatrix, lines, absoluteFilename, sb
-                                .toString(), depth, det < 0, destMatrix, DESTMatrix, datFile, parent.firstRef, readOnly, errorCheckOnly, alreadyParsed, parent, lineNumber)));
+                                .toString(), depth, det < 0, destMatrix, DESTMatrix, datFile, parent.firstRef, readOnly, errorCheckOnly, alreadyParsed, parent)));
                     }
 
                     // Avoid scaling of flat files
