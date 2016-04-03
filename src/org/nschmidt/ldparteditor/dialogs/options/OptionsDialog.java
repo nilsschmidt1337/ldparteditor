d4a7cf0a3d02a8436694041e1b9ccb859172c708
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2016-04-03 15:50:10 +0200

    Prepared fix for issue #155, #156, #157.

--- src/org/nschmidt/ldparteditor/dialogs/options/OptionsDialog.java
+++ src/org/nschmidt/ldparteditor/dialogs/options/OptionsDialog.java
@@ -1,5 +1,25 @@
 package org.nschmidt.ldparteditor.dialogs.options;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
+import org.nschmidt.ldparteditor.helpers.Version;
+import org.nschmidt.ldparteditor.resources.ResourceManager;
+
 public class OptionsDialog extends OptionsDesign {
+
+    public OptionsDialog(Shell parentShell) {
+        super(parentShell);
+    }
+
+    public void run() {
+        this.setBlockOnOpen(true);
+        this.setShellStyle(SWT.APPLICATION_MODAL | SWT.SHELL_TRIM ^ SWT.MIN);
+        this.create();
+        final Shell sh = this.getShell();
+        sh.setText(Version.getApplicationName());
+        sh.setImage(ResourceManager.getImage("imgDuke2.png")); //$NON-NLS-1$
+        this.open();
+    }
+
     // FIXME OptionsDialog needs implementation!
 }
