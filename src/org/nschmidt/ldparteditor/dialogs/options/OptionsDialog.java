8371cda8da39d4b7abfe0eaf960983753f69b30d
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2016-04-09 21:55:13 +0200

    Prepared fix for issue #155, #156, #157.

--- src/org/nschmidt/ldparteditor/dialogs/options/OptionsDialog.java
+++ src/org/nschmidt/ldparteditor/dialogs/options/OptionsDialog.java
@@ -1,6 +1,8 @@
 package org.nschmidt.ldparteditor.dialogs.options;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Shell;
 import org.nschmidt.ldparteditor.helpers.Version;
 import org.nschmidt.ldparteditor.resources.ResourceManager;
@@ -12,12 +14,21 @@ public class OptionsDialog extends OptionsDesign {
     }
 
     public void run() {
+        final OptionsDialog me = this;
         this.setBlockOnOpen(true);
         this.setShellStyle(SWT.APPLICATION_MODAL | SWT.SHELL_TRIM ^ SWT.MIN);
         this.create();
         final Shell sh = this.getShell();
         sh.setText(Version.getApplicationName());
         sh.setImage(ResourceManager.getImage("imgDuke2.png")); //$NON-NLS-1$
+
+        btn_OK[0].addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                me.close();
+            }
+        });
+
         this.open();
     }
 
