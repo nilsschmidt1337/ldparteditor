533f8774cf55bbd8ad39ad21ea93c778001983d0
Nils Schmidt <nils.schmidt@schmidthj.de>
2016-07-02 18:25:36 +0200

    Fixed issue #336.

--- src/org/nschmidt/ldparteditor/data/VM10Selector.java
+++ src/org/nschmidt/ldparteditor/data/VM10Selector.java
@@ -682,6 +682,32 @@ class VM10Selector extends VM09WindingChange {
                     }
 
                     clearVertexNormalCache();
+                    
+                    // Deselect excluded data types (vertices, lines, etc.)
+                    
+                    if (!ss.isVertices()) {
+                        selectedVertices.clear();
+                    }
+
+                    if (!ss.isLines()) {
+                        selectedData.removeAll(selectedLines);
+                        selectedLines.clear();                    
+                    }
+
+                    if (!ss.isTriangles()) {
+                        selectedData.removeAll(selectedTriangles);
+                        selectedTriangles.clear();
+                    }
+
+                    if (!ss.isQuads()) {
+                        selectedData.removeAll(selectedQuads);
+                        selectedQuads.clear();                      
+                    }
+
+                    if (!ss.isCondlines()) {
+                        selectedData.removeAll(selectedCondlines);
+                        selectedCondlines.clear();                      
+                    }
 
                     // Extend selection to whole subfiles
                     if (ss.isWholeSubfiles() && !ss.isNoSubfiles()) {
