337c2aa8750950b3959de8565c6da6fcf9b875a3
Nils Schmidt <nils.schmidt@schmidthj.de>
2016-07-17 23:17:05 +0200

    Prepared fix for issue #342.

--- src/org/nschmidt/ldparteditor/shells/editortext/EditorTextWindow.java
+++ src/org/nschmidt/ldparteditor/shells/editortext/EditorTextWindow.java
@@ -571,6 +571,7 @@ public class EditorTextWindow extends EditorTextDesign {
                     NLogger.debug(getClass(), "From line {0}", fromLine); //$NON-NLS-1$
                     NLogger.debug(getClass(), "To   line {0}", toLine); //$NON-NLS-1$
                     Text2SelectionConverter.convert(fromLine, toLine, selection.getState().getFileNameObj());
+                    st.redraw(0, 0, st.getBounds().width, st.getBounds().height, true);
                     st.forceFocus();
                 }
             }
