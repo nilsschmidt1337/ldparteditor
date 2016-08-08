d49338aec948f0f7d9c3f1f4448ecc558c11844a
Nils Schmidt <nils.schmidt@schmidthj.de>
2016-08-08 19:11:30 +0200

    Prepared fix for issue #363.

--- src/org/nschmidt/ldparteditor/dialogs/primgen2/PrimGen2Dialog.java
+++ src/org/nschmidt/ldparteditor/dialogs/primgen2/PrimGen2Dialog.java
@@ -26,11 +26,14 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Widget;
 import org.nschmidt.ldparteditor.data.GData;
 import org.nschmidt.ldparteditor.data.GDataCSG;
 import org.nschmidt.ldparteditor.data.VertexManager;
 import org.nschmidt.ldparteditor.project.Project;
 import org.nschmidt.ldparteditor.text.SyntaxFormatter;
+import org.nschmidt.ldparteditor.widgets.IntegerSpinner;
+import org.nschmidt.ldparteditor.widgets.ValueChangeAdapter;
 import org.nschmidt.ldparteditor.workbench.UserSettingState;
 import org.nschmidt.ldparteditor.workbench.WorkbenchManager;
 
@@ -46,6 +49,22 @@ import org.nschmidt.ldparteditor.workbench.WorkbenchManager;
  */
 public class PrimGen2Dialog extends PrimGen2Design {
 
+    private final int CIRCLE = 0;
+    private final int RING = 1;
+    private final int CONE = 2;
+    private final int TORUS = 3;
+    private final int CYLINDER = 4;
+    private final int DISC = 5;
+    private final int DISC_NEGATIVE = 6;
+    private final int CHORD = 7;
+    
+    private boolean doUpdate = false; 
+    
+    private enum EventType {
+        SPN,
+        CBO
+    }
+    
     // I18N Needs translation / i18n!
     
     // FIXME Needs implementation (Logic)!
@@ -100,11 +119,13 @@ public class PrimGen2Dialog extends PrimGen2Design {
                 sb.append("0 !LICENSE Redistributable under CCAL version 2.0 : see CAreadme.txt\n\n"); //$NON-NLS-1$
 
                 sb.append("0 BFC CERTIFY CCW\n\n"); //$NON-NLS-1$
+                
                 sb.append("2 24 1 0 0 0.9239 0 0.3827\n"); //$NON-NLS-1$
                 sb.append("2 24 0.9239 0 0.3827 0.7071 0 0.7071\n"); //$NON-NLS-1$
                 sb.append("2 24 0.7071 0 0.7071 0.3827 0 0.9239\n"); //$NON-NLS-1$
                 sb.append("2 24 0.3827 0 0.9239 0 0 1\n"); //$NON-NLS-1$
                 sb.append("0 // Build by Primitive Generator 2\n"); //$NON-NLS-1$
+                
                 txt_data[0].setText(sb.toString());
             }
         });
@@ -162,14 +183,9 @@ public class PrimGen2Dialog extends PrimGen2Design {
         cmb_type[0].addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                final int CIRCLE = 0;
-                final int RING = 1;
-                final int CONE = 2;
-                final int TORUS = 3;
-                final int CYLINDER = 4;
-                final int DISC = 5;
-                final int DISC_NEGATIVE = 6;
-                final int CHORD = 7;
+                
+                doUpdate = true;
+                        
                 switch (cmb_type[0].getSelectionIndex()) {
                 case CIRCLE:
 
@@ -198,8 +214,37 @@ public class PrimGen2Dialog extends PrimGen2Design {
                 default:
                     break;
                 }
+                
+                doUpdate = false;
+                
+                rebuildPrimitive(EventType.CBO, cmb_type[0]);
             }
         });
+        
+        final SelectionAdapter sa = new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                rebuildPrimitive(EventType.CBO, e.widget);
+            }
+        };
+        
+        final ValueChangeAdapter vca = new ValueChangeAdapter() {
+            @Override
+            public void valueChanged(IntegerSpinner spn) {
+                rebuildPrimitive(EventType.SPN, spn);
+            }
+        };
+        
+        spn_divisions[0].addValueChangeListener(vca);
+        spn_major[0].addValueChangeListener(vca);
+        spn_minor[0].addValueChangeListener(vca);
+        spn_segments[0].addValueChangeListener(vca);
+        spn_size[0].addValueChangeListener(vca);
+        
+        cmb_divisions[0].addSelectionListener(sa);
+        cmb_segments[0].addSelectionListener(sa);
+        cmb_torusType[0].addSelectionListener(sa);
+        cmb_winding[0].addSelectionListener(sa);
         
         int result = super.open();
         Project.getUnsavedFiles().remove(df);        
@@ -211,6 +256,126 @@ public class PrimGen2Dialog extends PrimGen2Design {
     protected void handleShellCloseEvent() {
         c3d.getRenderer().disposeAllTextures();
         super.handleShellCloseEvent();
+    }
+    
+    private void rebuildPrimitive(EventType et, Widget w) {
+        
+        if (doUpdate) {
+            return;
+        }
+        
+        boolean ccw = cmb_winding[0].getSelectionIndex() == 0;        
+        boolean inner = cmb_torusType[0].getSelectionIndex() == 0;
+        
+        doUpdate = true;
+        
+        if (w != spn_divisions[0] && w != cmb_divisions[0] && spn_segments[0].getValue() > spn_divisions[0].getValue()) {
+            spn_divisions[0].setValue(spn_segments[0].getValue());
+        }
+        
+        if (w != spn_segments[0] && w != cmb_segments[0] && spn_segments[0].getValue() > spn_divisions[0].getValue()) {
+            spn_segments[0].setValue(spn_divisions[0].getValue());
+        }
+        
+        int divisions = spn_divisions[0].getValue();
+        int segments = spn_segments[0].getValue();
+        int major = spn_major[0].getValue();
+        int minor = spn_minor[0].getValue();
+        int size = spn_size[0].getValue();
+        
+        if (et == EventType.CBO) {
+            
+            switch (cmb_segments[0].getSelectionIndex()) {
+            case 0:
+                spn_segments[0].setValue(16);
+                break;
+            case 1:
+                spn_segments[0].setValue(48);
+                break;
+            default:
+                break;
+            }
+            switch (cmb_divisions[0].getSelectionIndex()) {
+            case 0:
+                spn_divisions[0].setValue(spn_segments[0].getValue() / 4);
+                break;
+            case 1:
+                spn_divisions[0].setValue(spn_segments[0].getValue() / 2);
+                break;
+            case 2:
+                spn_divisions[0].setValue(spn_segments[0].getValue() * 3 / 4);
+                break;
+            case 3:
+                spn_divisions[0].setValue(spn_segments[0].getValue());
+                break;
+            default:
+                break;
+            }
+            
+        } else {
+            
+            switch (spn_divisions[0].getValue()) {
+            case 8:
+                cmb_divisions[0].select(0);
+                break;
+            case 16:
+                cmb_divisions[0].select(1);
+                break;
+            case 48:
+                cmb_divisions[0].select(2);
+                break;
+            default:
+                cmb_divisions[0].select(3);
+                break;
+            }
+            final int QUARTER = spn_divisions[0].getValue() / 4; 
+            final int HALF = spn_divisions[0].getValue() / 2;
+            final int THREE_OF_FOUR = spn_divisions[0].getValue() * 3 / 4;
+            final int WHOLE = spn_divisions[0].getValue();
+            
+            if (segments == QUARTER) {
+                cmb_segments[0].select(0);
+            } else if (segments == HALF) {
+                cmb_segments[0].select(1);
+            } else if (segments == THREE_OF_FOUR) {
+                cmb_segments[0].select(2);
+            } else if (segments == WHOLE) {
+                cmb_segments[0].select(3);
+            } else {
+                cmb_segments[0].select(4);
+            }
+            
+        }
+        doUpdate = false;
+        
+        switch (cmb_type[0].getSelectionIndex()) {
+        case CIRCLE:
+
+            break;
+        case RING:
+
+            break;
+        case CONE:
+
+            break;
+        case TORUS:
+
+            break;
+        case CYLINDER:
+
+            break;
+        case DISC:
+
+            break;
+        case DISC_NEGATIVE:
+
+            break;
+        case CHORD:
+
+            break;
+        default:
+            break;
+        }
     }
 
 }
