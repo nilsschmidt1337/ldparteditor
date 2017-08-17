eaebe1c7863e6b8a0d2c7bc2f35c1ae12c9497ec
Nils Schmidt <nils.schmidt@schmidthj.de>
2017-08-17 22:13:07 +0200

    Prepared fix for issue #149.

--- src/org/nschmidt/ldparteditor/widgets/NButton.java
+++ src/org/nschmidt/ldparteditor/widgets/NButton.java
@@ -24,6 +24,7 @@ import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.Point;
@@ -31,6 +32,7 @@ import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.wb.swt.SWTResourceManager;
+import org.nschmidt.ldparteditor.enums.Font;
 
 /**
  * @author nils
@@ -40,20 +42,28 @@ public class NButton extends Canvas {
 
     private final List<SelectionListener> selectors = new ArrayList<>();
     private final List<PaintListener> painters = new ArrayList<>();
+    private final boolean canToggle;
+
 
     private Image img = null;
     private String text = ""; //$NON-NLS-1$
     private boolean hovered = false;
     private boolean pressed = false;
+    private boolean selected = false;
 
 
     public NButton(Composite parent, int style) {
         super(parent, style);
 
+        canToggle = (style & SWT.TOGGLE) == SWT.TOGGLE;
+
         super.addPaintListener(this::paint);
 
         addListener(SWT.MouseDown, event -> {
             pressed = true;
+            if (canToggle) {
+                selected = !selected;
+            }
             redraw();
             update();
             final SelectionEvent se = new SelectionEvent(event);
@@ -116,26 +126,47 @@ public class NButton extends Canvas {
     }
 
     private void paint(PaintEvent event) {
+        final GC gc = event.gc;
         final Image img = this.img;
+        final boolean focused = this.isFocusControl();
         final boolean hasImage = img != null;
         final int img_width = hasImage ? img.getImageData().width : 0;
         final int img_height = hasImage ? img.getImageData().height : 0;
 
-        event.gc.setForeground(SWTResourceManager.getColor(60, 60, 60));
+
+        gc.setFont(Font.SYSTEM);
+        // setFont before using textExtent, so that the size of the text
+        // can be calculated correctly
+        final Point textExtent = gc.textExtent(getText());
+
+        // TODO 1. Calculate sizes
+
+
+        // TODO 2. Draw Content
+
+
+        gc.setForeground(SWTResourceManager.getColor(60, 60, 60));
 
         if (hovered) {
-            event.gc.setForeground(SWTResourceManager.getColor(255, 255, 255));
+            gc.setForeground(SWTResourceManager.getColor(255, 255, 255));
         }
         if (pressed) {
-            event.gc.setForeground(SWTResourceManager.getColor(30, 30, 30));
+            gc.setForeground(SWTResourceManager.getColor(30, 30, 30));
         }
 
-        event.gc.drawRoundRectangle(0, 0, img_width + 9, img_height + 9, 5, 5);
+        gc.drawRoundRectangle(0, 0, img_width + 9, img_height + 9, 5, 5);
 
         if (hasImage) {
 
-            event.gc.drawImage(img, 5, 5);
+            gc.drawImage(img, 5, 5);
         }
+
+        gc.setForeground(getForeground());
+
+        gc.drawString(getText(), 0, 0, true);
+
+
+        // 3. Paint custom forms
         for (PaintListener pl : painters) {
             pl.paintControl(event);
         }
@@ -143,6 +174,13 @@ public class NButton extends Canvas {
 
     @Override
     public Point computeSize(int wHint, int hHint, boolean changed) {
+
+        final GC gc = new GC(this);
+        gc.setFont(Font.SYSTEM);
+        // setFont before using textExtent, so that the size of the text
+        // can be calculated correctly
+        final Point textExtent = gc.textExtent(getText());
+        gc.dispose();
 
         if (img != null) {
             ImageData data = img.getImageData();
@@ -155,10 +193,18 @@ public class NButton extends Canvas {
     }
 
     public String getText() {
+        checkWidget();
         return text;
     }
 
+    public boolean getSelection() {
+        checkWidget();
+        return selected;
+    }
+
     public void setText(String text) {
+        checkWidget();
         this.text = text;
+        redraw();
     }
 }
