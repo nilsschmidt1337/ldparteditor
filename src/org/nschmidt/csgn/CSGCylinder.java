88a8b47d00b7e17924f60b878ceaee57ae60cca4
Nils Schmidt <nils.schmidt@schmidthj.de>
2017-08-25 18:34:42 +0200

    Prepared fix for issue #601.

--- src/org/nschmidt/csgn/CSGCylinder.java
+++ src/org/nschmidt/csgn/CSGCylinder.java
@@ -1,3 +1,18 @@
+/* MIT - License
+
+Copyright (c) 2012 - this year, Nils Schmidt
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 package org.nschmidt.csgn;
 
 import java.util.ArrayList;
@@ -11,22 +26,7 @@ public class CSGCylinder extends CSGPrimitive implements Primitive {
 
     public final int ID = id_counter.getAndIncrement();
 
-    private Vector3d start;
-    private Vector3d end;
-    private double radius;
-    private int numSlices;
-
-    /**
-     * Constructor. Creates a new cylinder with center {@code [0,0,0]} and
-     * ranging from {@code [0,-0.5,0]} to {@code [0,0.5,0]}, i.e.
-     * {@code size = 1}.
-     */
-    public CSGCylinder() {
-        this.start = new Vector3d(0, 0d, 0);
-        this.end = new Vector3d(0, 1000d, 0);
-        this.radius = 1000d;
-        this.numSlices = 16;
-    }
+    private final int numSlices;
 
     /**
      * Constructor. Creates a cylinder . The resolution of the tessellation can
@@ -36,65 +36,36 @@ public class CSGCylinder extends CSGPrimitive implements Primitive {
      *            number of slices (used for tessellation)
      */
     public CSGCylinder(int numSlices) {
-        this.start = new Vector3d(0, 0d, 0);
-        this.end = new Vector3d(0, 1000d, 0);
-        this.radius = 1000d;
-        this.numSlices = numSlices;
-    }
-
-    /**
-     * Constructor. Creates a cylinder ranging from {@code [0,0,0]} to
-     * {@code [0,0,height]} with the specified {@code radius} and {@code height}
-     * . The resolution of the tessellation can be controlled with
-     * {@code numSlices}.
-     *
-     * @param radius
-     *            cylinder radius
-     * @param height
-     *            cylinder height
-     * @param numSlices
-     *            number of slices (used for tessellation)
-     */
-    public CSGCylinder(double radius, double height, int numSlices) {
-        this.start = Vector3d.ZERO;
-        this.end = Vector3d.Z_ONE.times(height);
-        this.radius = radius;
         this.numSlices = numSlices;
     }
 
     @Override
     public List<Triangle> toTriangles(DatFile df, GColour colour) {
-        final Vector3d s = getStart();
-        Vector3d e = getEnd();
-        final Vector3d ray = e.minus(s);
-        final Vector3d axisZ = ray.unit();
-        boolean isY = Math.abs(axisZ.y) > 0.5;
-        final Vector3d axisX = new Vector3d(isY ? 1 : 0, !isY ? 1 : 0, 0).cross(axisZ).unit();
+        final Vector3d axisZ = new Vector3d(0, 1d, 0);
+        final Vector3d axisX = new Vector3d(1, 0, 0).cross(axisZ).unit();
         final Vector3d axisY = axisX.cross(axisZ).unit();
-        Vector3d startV = s.clone();
-        Vector3d endV = e.clone();
         List<Triangle> polygons = new ArrayList<>();
 
         for (int i = 0; i < numSlices; i++) {
             double t0 = i / (double) numSlices, t1 = (i + 1) / (double) numSlices;
             {
-                Vector3d v1 = startV;
-                Vector3d v2 = cylPoint(axisX, axisY, axisZ, ray, s, radius, 0, t0, -1);
-                Vector3d v3 = cylPoint(axisX, axisY, axisZ, ray, s, radius, 0, t1, -1);
+                Vector3d v1 = new Vector3d(0, 0, 0);
+                Vector3d v2 = cylPoint(axisX, axisY, 0, t0, -1);
+                Vector3d v3 = cylPoint(axisX, axisY, 0, t1, -1);
                 polygons.add(new Triangle(df, v1, v2, v3, new GColourIndex(colour, ID)));
             }
             {
-                Vector3d v1 = cylPoint(axisX, axisY, axisZ, ray, s, radius, 0, t1, 0);
-                Vector3d v2 = cylPoint(axisX, axisY, axisZ, ray, s, radius, 0, t0, 0);
-                Vector3d v3 = cylPoint(axisX, axisY, axisZ, ray, s, radius, 1, t0, 0);
-                Vector3d v4 = cylPoint(axisX, axisY, axisZ, ray, s, radius, 1, t1, 0);
+                Vector3d v1 = cylPoint(axisX, axisY, 0, t1, 0);
+                Vector3d v2 = cylPoint(axisX, axisY, 0, t0, 0);
+                Vector3d v3 = cylPoint(axisX, axisY, 1000d, t0, 0);
+                Vector3d v4 = cylPoint(axisX, axisY, 1000d, t1, 0);
                 polygons.add(new Triangle(df, v1, v2, v3, new GColourIndex(colour, ID)));
                 polygons.add(new Triangle(df, v3, v4, v1, new GColourIndex(colour, ID)));
             }
             {
-                Vector3d v1 = endV;
-                Vector3d v2 = cylPoint(axisX, axisY, axisZ, ray, s, radius, 1, t1, 1);
-                Vector3d v3 =  cylPoint(axisX, axisY, axisZ, ray, s, radius, 1, t0, 1);
+                Vector3d v1 = new Vector3d(0, 1000d, 0);
+                Vector3d v2 = cylPoint(axisX, axisY, 1000d, t1, 1);
+                Vector3d v3 =  cylPoint(axisX, axisY, 1000d, t0, 1);
                 polygons.add(new Triangle(df, v1, v2, v3, new GColourIndex(colour, ID)));
             }
         }
@@ -102,71 +73,11 @@ public class CSGCylinder extends CSGPrimitive implements Primitive {
         return polygons;
     }
 
-    private Vector3d cylPoint(Vector3d axisX, Vector3d axisY, Vector3d axisZ, Vector3d ray, Vector3d s, double r, double stack, double slice, double normalBlend) {
+    private Vector3d cylPoint(Vector3d axisX, Vector3d axisY, double stack, double slice, double normalBlend) {
         double angle = slice * Math.PI * 2;
         Vector3d out = axisX.times(Math.cos(angle)).plus(axisY.times(Math.sin(angle)));
-        Vector3d pos = s.plus(ray.times(stack)).plus(out.times(r));
+        Vector3d pos = new Vector3d(0, stack, 0).plus(out.times(1000d));
         return pos;
-    }
-
-    /**
-     * @return the start
-     */
-    public Vector3d getStart() {
-        return start;
-    }
-
-    /**
-     * @param start
-     *            the start to set
-     */
-    public void setStart(Vector3d start) {
-        this.start = start;
-    }
-
-    /**
-     * @return the end
-     */
-    public Vector3d getEnd() {
-        return end;
-    }
-
-    /**
-     * @param end
-     *            the end to set
-     */
-    public void setEnd(Vector3d end) {
-        this.end = end;
-    }
-
-    /**
-     * @return the radius
-     */
-    public double getRadius() {
-        return radius;
-    }
-
-    /**
-     * @param radius
-     *            the radius to set
-     */
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    /**
-     * @return the number of slices
-     */
-    public int getNumSlices() {
-        return numSlices;
-    }
-
-    /**
-     * @param numSlices
-     *            the number of slices to set
-     */
-    public void setNumSlices(int numSlices) {
-        this.numSlices = numSlices;
     }
 
     @Override
--- src/org/nschmidt/csgn/Vector3d.java
+++ src/org/nschmidt/csgn/Vector3d.java
@@ -1,3 +1,36 @@
+/**
+ * Vector3d.java
+ *
+ * Copyright 2014-2014 Michael Hoffer <info@michaelhoffer.de>. All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Michael Hoffer <info@michaelhoffer.de> "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL Michael Hoffer <info@michaelhoffer.de> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Michael Hoffer
+ * <info@michaelhoffer.de>.
+ */
 package org.nschmidt.csgn;
 
 public class Vector3d {
@@ -141,7 +174,7 @@ public class Vector3d {
      *         the point midway between this and the specified vector if
      *         {@code t = 0.5}
      */
-    public Vector3d lerp(Vector3d a, double t) {
+    public Vector3d interpolate(Vector3d a, double t) {
         return this.plus(a.minus(this).times(t));
     }
 
@@ -214,20 +247,5 @@ public class Vector3d {
         hash = 97 * hash + (int) (Double.doubleToLongBits(this.y) ^ Double.doubleToLongBits(this.y) >>> 32);
         hash = 97 * hash + (int) (Double.doubleToLongBits(this.z) ^ Double.doubleToLongBits(this.z) >>> 32);
         return hash;
-    }
-
-
-    /**
-     * Create a new vertex between this vertex and the specified vertex by
-     * linearly interpolating all properties using a parameter t.
-     *
-     * @param other
-     *            vertex
-     * @param t
-     *            interpolation parameter
-     * @return a new vertex between this and the specified vertex
-     */
-    public Vector3d interpolate(Vector3d other, double t) {
-        return this.lerp(other, t);
     }
 }
