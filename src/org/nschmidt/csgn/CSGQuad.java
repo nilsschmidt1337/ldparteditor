96b2ed39521204af8d85079acc3ffe65cfd9bb6b
Nils Schmidt <nils.schmidt@schmidthj.de>
2017-08-25 19:19:48 +0200

    Prepared fix for issue #601.

--- src/org/nschmidt/csgn/CSGQuad.java
+++ src/org/nschmidt/csgn/CSGQuad.java
@@ -31,13 +31,13 @@ public class CSGQuad extends CSGPrimitive implements Primitive {
 
         List<Triangle> triangles = new ArrayList<>();
         // position // normal
-        int[][] info = { { 2, 6, 7, 3 }, { 0, 1, 0 } } ;
+        int[][] info = { { 3, 7, 6, 2 }, { 0, 1, 0 } } ;
 
         List<Vector3d> vertices = new ArrayList<Vector3d>();
         for (int i : info[0]) {
             vertices.add(new Vector3d(
                     2000d * (1 * Math.min(1, i & 1) - 0.5),
-                    (Math.min(1, i & 2) - 0.5),
+                    0d,
                     2000d * (Math.min(1, i & 4) - 0.5)));
         }
         triangles.add(new Triangle(df, vertices.get(0), vertices.get(1), vertices.get(2), new GColourIndex(colour, ID)));
