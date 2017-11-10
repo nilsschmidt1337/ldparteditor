70aa76a4ca14079e856480d920fc2173933f70f9
Nils Schmidt <nils.schmidt@schmidthj.de>
2017-11-10 20:01:35 +0100

    Prepared fix for issue #601.

--- src/org/nschmidt/csg/CSGOptimizerEdgeCollapse.java
+++ src/org/nschmidt/csg/CSGOptimizerEdgeCollapse.java
@@ -5,16 +5,29 @@ import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
 
+import org.nschmidt.ldparteditor.data.GColour;
+import org.nschmidt.ldparteditor.data.GData1;
 import org.nschmidt.ldparteditor.data.GData3;
+import org.nschmidt.ldparteditor.data.Vertex;
+import org.nschmidt.ldparteditor.enums.View;
 
 enum CSGOptimizerEdgeCollapse {
     INSTANCE;
 
     public static boolean optimize(Random rnd, Map<Plane, List<GData3>> trianglesPerPlane, TreeMap<GData3, IdAndPlane> optimization) {
+        boolean result = false;
 
         // FIXME Do iterative optimization here!
 
-        boolean result = false;
         return result;
+    }
+
+    private static GData3 createTriangle(GData3 idol, VectorCSGd a, VectorCSGd b, VectorCSGd c, int col) {
+        Vertex v1 = new Vertex((float) a.x, (float) a.y, (float) a.z);
+        Vertex v2 = new Vertex((float) b.x, (float) b.y, (float) b.z);
+        Vertex v3 = new Vertex((float) c.x, (float) c.y, (float) c.z);
+        GData1 parent = idol.parent;
+        GColour colour = View.getLDConfigColour(col); // new GColour(idol.colourNumber, idol.r, idol.g, idol.b, idol.a);
+        return new GData3(v1, v2, v3, parent, colour, true);
     }
 }
