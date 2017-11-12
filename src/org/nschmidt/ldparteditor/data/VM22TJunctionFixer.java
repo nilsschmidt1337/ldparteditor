ce65dcd880af22d755a5827100dea5d8227fd267
Nils Schmidt <nils.schmidt@schmidthj.de>
2017-11-12 13:48:16 +0100

    Fixed issue #609.

--- src/org/nschmidt/ldparteditor/data/VM22TJunctionFixer.java
+++ src/org/nschmidt/ldparteditor/data/VM22TJunctionFixer.java
@@ -88,7 +88,7 @@ class VM22TJunctionFixer extends VM21Merger {
                                         clearSelection2();
                                         selectedVertices.add(v);
                                         verticesToSelect.add(v);
-                                        if (doMerge) merge(MergeTo.NEAREST_EDGE, false, false);
+                                        if (doMerge) merge(MergeTo.NEAREST_EDGE_SPLIT, false, false);
                                         TjunctionCount[0]++;
                                     }
                                     monitor.worked(1);
