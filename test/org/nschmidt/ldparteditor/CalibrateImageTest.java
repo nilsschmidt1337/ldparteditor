044367ec5a0df3c94178af349718ac0d3067e524
Nils Schmidt <nilsschmidt1337@users.noreply.github.com>
2024-06-27 22:17:01 +0200

    Prepared fix for issue #1048.

--- test/org/nschmidt/ldparteditor/CalibrateImageTest.java
+++ test/org/nschmidt/ldparteditor/CalibrateImageTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
@@ -15,7 +14,9 @@ import org.nschmidt.ldparteditor.data.Vertex;
 @SuppressWarnings("java:S5960")
 public class CalibrateImageTest {
 
-    private static final Matrix4f ID = Matrix4f.setIdentity(new Matrix4f());
+    private static Matrix4f id() {
+        return Matrix4f.setIdentity(new Matrix4f());
+    }
 
     private Vector4f[] calibrate(Matrix4f imageMatrix, final Vector4f imageOffset,
             float angleA, float angleB, float angleC,
@@ -26,47 +27,21 @@ public class CalibrateImageTest {
         Matrix4f.rotate((float) (angleB / 180.0 * Math.PI), new Vector3f(1f, 0f, 0f), imageBasis, imageBasis);
         Matrix4f.rotate((float) (angleA / 180.0 * Math.PI), new Vector3f(0f, 1f, 0f), imageBasis, imageBasis);
 
-        imageMatrix = imageMatrix.scale(new Vector3f(1E-3f, 1E-3f, 1E-3f));
-        imageMatrix.m30 = 0f;
-        imageMatrix.m31 = 0f;
-        imageMatrix.m32 = 0f;
-        imageBasis = repairSingularMatrix(imageBasis);
         imageBasis = Matrix4f.invert(imageBasis, null);
 
-        imageMatrix = imageMatrix.scale(new Vector3f(scale, scale, scale));
-        final Vector4f pivotOnImage = pivot;// Matrix4f.transform(imageMatrix, pivot, null);
-        final Vector4f newImageOffset = new Vector4f((imageOffset.x - pivotOnImage.x) * scale + pivotOnImage.x, (imageOffset.y - pivotOnImage.y) * scale + pivotOnImage.y, (imageOffset.z - pivotOnImage.z) * scale + pivotOnImage.z, 0f);
+        final Vector4f realImageOffset = (Vector4f) new Vector4f(imageMatrix.m30, imageMatrix.m31, imageMatrix.m32, 1000f).scale(.001f);
+        final Vector4f pivotToImageCenter = Vector4f.sub(pivot, realImageOffset, null);
+        final Vector4f pivotToImageCenterScaled = (Vector4f) pivotToImageCenter.scale(scale).scale(.5f);
+        final Vector4f offsetDelta = (Vector4f) Matrix4f.transform(imageBasis, pivotToImageCenterScaled, null).negate();
+
+        final Vector4f newImageOffset = Vector4f.add(imageOffset, offsetDelta, null);
         final Vector4f newImageScale = new Vector4f(imageScale.x * scale, imageScale.y * scale, 0f, 0f);
         return new Vector4f[] {newImageOffset, newImageScale};
     }
 
-    private Matrix4f repairSingularMatrix(Matrix4f imageMatrix) {
-        boolean m00 = Float.compare(imageMatrix.m00, 0f) == 0;
-        boolean m01 = Float.compare(imageMatrix.m01, 0f) == 0;
-        boolean m02 = Float.compare(imageMatrix.m02, 0f) == 0;
-        boolean m10 = Float.compare(imageMatrix.m10, 0f) == 0;
-        boolean m11 = Float.compare(imageMatrix.m11, 0f) == 0;
-        boolean m12 = Float.compare(imageMatrix.m12, 0f) == 0;
-        boolean m20 = Float.compare(imageMatrix.m20, 0f) == 0;
-        boolean m21 = Float.compare(imageMatrix.m21, 0f) == 0;
-        boolean m22 = Float.compare(imageMatrix.m22, 0f) == 0;
-
-        if (m00 && (m01 && m02 || m10 && m20)) {
-            imageMatrix.m00 = 1f;
-        }
-        if (m11 && (m10 && m12 || m01 && m21)) {
-            imageMatrix.m11 = 1f;
-        }
-        if (m22 && (m20 && m21 || m02 && m12)) {
-            imageMatrix.m22 = 1f;
-        }
-
-        return imageMatrix;
-    }
-
     @Test
     public void testImageScaleByTwoWithZeroOrigin() {
-         Vector4f[] result = calibrate(ID, new Vector4f(), 0f, 0f, 0f, new Vector4f(1f, 1f, 0f, 1f), new Vector4f(), 2f);
+         Vector4f[] result = calibrate(id(), new Vector4f(), 0f, 0f, 0f, new Vector4f(1f, 1f, 0f, 1f), new Vector4f(), 2f);
          final Vector4f offset = result[0];
          final Vector4f scale = result[1];
          assertEquals(0f, offset.x, 0f);
@@ -78,7 +53,7 @@ public class CalibrateImageTest {
 
     @Test
     public void testImageScaleByTwoWithShiftedOrigin() {
-         Vector4f[] result = calibrate(ID, new Vector4f(2f, 4f, 8f, 1f), 0f, 0f, 0f, new Vector4f(1f, 1f, 0f, 1f), new Vector4f(), 2f);
+         Vector4f[] result = calibrate(id().translate(new Vector3f(2000f, 4000f, 8000f)), new Vector4f(2f, 4f, 8f, 1f), 0f, 0f, 0f, new Vector4f(1f, 1f, 0f, 1f), new Vector4f(), 2f);
          final Vector4f offset = result[0];
          final Vector4f scale = result[1];
          assertEquals(4f, offset.x, 0f);
@@ -90,7 +65,7 @@ public class CalibrateImageTest {
 
     @Test
     public void testImageScaleByTwoWithShiftedPivot() {
-         Vector4f[] result = calibrate(ID, new Vector4f(0f, 0f, 0f, 1f), 0f, 0f, 0f, new Vector4f(1f, 1f, 0f, 1f), new Vector4f(1f, 1f, 0f, 1f), 2f);
+         Vector4f[] result = calibrate(id(), new Vector4f(0f, 0f, 0f, 1f), 0f, 0f, 0f, new Vector4f(1f, 1f, 0f, 1f), new Vector4f(1f, 1f, 0f, 1f), 2f);
          final Vector4f offset = result[0];
          final Vector4f scale = result[1];
          assertEquals(-1f, offset.x, 0f);
@@ -101,23 +76,6 @@ public class CalibrateImageTest {
     }
 
     @Test
-    public void testImageScaleByTwoWithShiftedPivotAndUndoAgain() {
-        Vector4f[] firstResult = calibrate(ID, new Vector4f(0f, 0f, 0f, 1f), 0f, 0f, 0f, new Vector4f(1f, 1f, 0f, 1f), new Vector4f(1f, 1f, 0f, 1f), 2f);
-        final Vector4f firstOffset = firstResult[0];
-        final Vector4f firstScale = firstResult[1];
-        Vector4f[] finalResult = calibrate(ID, new Vector4f(firstOffset.x, firstOffset.y, firstOffset.z, 1f), 0f, 0f, 0f, new Vector4f(firstScale.x, firstScale.y, 0f, 1f), new Vector4f(1f, 1f, 0f, 1f), 0.5f);
-        final Vector4f offset = finalResult[0];
-        final Vector4f scale = finalResult[1];
-        assertEquals(0f, offset.x, 0f);
-        assertEquals(0f, offset.y, 0f);
-        assertEquals(0f, offset.z, 0f);
-        assertEquals(1f, scale.x, 0f);
-        assertEquals(1f, scale.y, 0f);
-    }
-
-    @Test
-    @Ignore
-    // TODO Remove ignore asap
     public void testImageScaleComplexExample() {
         final GDataPNG png = new GDataPNG("0 !LPE PNG 5 1 0 90 90 0 10 10 test.png", //$NON-NLS-1$
             new Vertex(BigDecimal.valueOf(5), BigDecimal.ONE, BigDecimal.ZERO),
@@ -128,10 +86,10 @@ public class CalibrateImageTest {
         Vector4f[] result = calibrate(maxtrix, new Vector4f(5f, 1f, 0f, 1f), 90f, 90f, 0f, new Vector4f(10f, 10f, 0f, 1f), new Vector4f(0f, 5f, 1f, 1f), 2f);
         final Vector4f offset = result[0];
         final Vector4f scale = result[1];
-        assertEquals(5f, offset.x, 0f);
-        assertEquals(1f, offset.y, 0f);
-        assertEquals(0f, offset.z, 0f);
-        assertEquals(20f, scale.x, 0f);
-        assertEquals(20f, scale.y, 0f);
+        assertEquals(5f, offset.x, 0.001f);
+        assertEquals(1f, offset.y, 0.001f);
+        assertEquals(0f, offset.z, 0.001f);
+        assertEquals(20f, scale.x, 0.001f);
+        assertEquals(20f, scale.y, 0.001f);
     }
 }
